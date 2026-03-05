cd008e72bab15c006bad907792207bfc9f2a833c
dawrandev <dawrandev@gmail.com>
2026-03-05 12:33:59 +0500

    Project title is changed

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -10,7 +10,7 @@
     <meta name="author" content="{{ __('pixelstrap') }}">
     <link rel="icon" href="../assets/images/favicon.png" type="image/x-icon">
     <link rel="shortcut icon" href="../assets/images/favicon.png" type="image/x-icon">
-    <title>{{ __('Cuba - Премиум шаблон админ-панели') }}</title>
+    <title>{{ __('QR Gastranomic Quality') }}</title>
     <link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i&amp;display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900&amp;display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="../assets/css/font-awesome.css">
